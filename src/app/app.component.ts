86d9c8e397e6acfd03a217322681e6d50bfe7925
Magdalena Sremska <m.sremska@cheil.com>
2020-12-29 17:23:17 +0100

    angular app & bootstrap styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'to-do-list';
+  editMode = false;
+  taskName = 'Sugerowane zadanie codzienne: odkurzanie';
+  taskDate = '';
+  config: { [key: string]: string } = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: '© Lista zadań,All rights reserved.',
+        date: new Date().toDateString(),
+      };
+    }, 500);
+    this.sortTasks();
+  }
+
+  clearTasks() {
+    this.tasks = [];
+  }
+
+  createTask() {
+    const task: Task = {
+      name: this.taskName,
+      deadline: this.taskDate,
+      done: false,
+    };
+    this.tasks.push(task);
+    this.taskName = '';
+    this.taskDate = '';
+    this.sortTasks();
+  }
+
+  switchEditMode() {
+    this.editMode = !this.editMode;
+  }
+
+  markTaskAsDone(task: Task) {
+    task.done = true;
+    this.sortTasks();
+  }
+
+  deleteTask(task: Task) {
+    this.tasks = this.tasks.filter((e) => e !== task);
+    this.sortTasks();
+  }
+
+  private sortTasks() {
+    this.tasks = this.tasks.sort((a: Task, b: Task) =>
+      a.done === b.done ? 0 : a.done ? 1 : -1
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import localePL from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+registerLocaleData(localePL);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
